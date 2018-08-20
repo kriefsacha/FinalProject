8231b4a06e432be684141405f45722b7e327a5c1
kriefsacha <35034421+kriefsacha@users.noreply.github.com>
2018-08-20 12:27:33 +0300

    Bug

--- FinalProject/Server/Startup.cs
+++ FinalProject/Server/Startup.cs
@@ -14,10 +14,15 @@ namespace Server
     {
         public void Configuration(IAppBuilder app)
         {
-            GlobalHost.DependencyResolver.Register(typeof(IDAL), () => new Manager());
-            GlobalHost.DependencyResolver.Register(typeof(ILogic), () => new Logic((IDAL)GlobalHost.DependencyResolver.GetService(typeof(IDAL))));
+            GlobalHost.DependencyResolver.Register(typeof(IDAL), () => 
+            new Manager()); //3
+
+            GlobalHost.DependencyResolver.Register(typeof(ILogic), () => 
+            new Logic((IDAL)GlobalHost.DependencyResolver.GetService(typeof(IDAL)))); //2
+
             GlobalHost.DependencyResolver.Register(
-                typeof(AirportHub), () => new AirportHub((ILogic)GlobalHost.DependencyResolver.GetService(typeof(ILogic))));
+                typeof(AirportHub), () => new AirportHub((ILogic)GlobalHost.DependencyResolver.GetService(typeof(ILogic)))); //1
+
             app.MapSignalR();
         }
     }
